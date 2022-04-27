52c01ccbd542ffe1fc0cf03fce4cdf2debbb6815
vainero <42108639+vainero@users.noreply.github.com>
2022-04-27 05:12:20 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-function compute()
+function checkdata()
+// Function returns false if Amount entered is less than or equal to zero
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal");
+    if (principal.value<=0){
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }
+    return true;
 }
-        
+function showrate(x)
+// Function updates span with final value from slider
+{
+    document.getElementById("enteredRate").innerText = x;
+}
+function compute()
+// Function determines interest earned an enters text under id=result
+{
+    var principal = parseFloat(document.getElementById("principal").value,10);
+//    var rate = parseFloat(document.getElementById("rate").value,10);
+    var rate = parseFloat(document.getElementById("enteredrate").value,10);
+    var years = parseFloat(document.getElementById("years").value,10);
+    var interest = principal * years * rate / 100;
+    var currentYear = new Date().getFullYear();
+    var finalYear = currentYear + years;
+    document.getElementById("result").innerHTML = "<br />If you deposit <mark>"+principal+"</mark>,<br /> at an interest rate of <mark>"+rate+"%</mark>.<br />You will receive an amount of <mark>"+interest+"</mark><br />in the year <mark>"+finalYear+"</mark>";
+}
